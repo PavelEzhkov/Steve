6054df105e023c2eb21383c88a84474c42338825
PavelEzhkov <pachaejk@gmail.com>
2019-04-12 16:22:41 +0300

    version 0.0.2

--- src/main/java/com.javacore.steve/commands/CommandWeather.java
+++ src/main/java/com.javacore.steve/commands/CommandWeather.java
@@ -1,17 +1,27 @@
 package com.javacore.steve.commands;
 
+
+import com.javacore.steve.helpers.WeatherHelper;
+
 public class CommandWeather extends ACommand {
     CommandWeather(String name, String description) {
         super(name, description);
     }
 
+
+
     @Override
     public void execute() {
+
+        WeatherHelper helper = new WeatherHelper();
+       /*
         try {
             Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("I can't do it yet");
+        System.out.println("I can't do it yet");*/
     }
+
+
 }
---
+++ src/main/java/com.javacore.steve/helpers/WeatherHelper.java
@@ -0,0 +1,52 @@
+package com.javacore.steve.helpers;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeatherHelper {
+
+
+    public static Map<String,Object> jsonToMap(String string){
+        Map<String, Object> map = new Gson().fromJson(string, new TypeToken<HashMap<String,Object>>(){}.getType());
+        return map;
+    }
+
+    private static final String API_KEY ="4f850c64b1fab5268c29242452d686ab";
+    private static final String LOCATION ="Saint%20Petersburg,Ru";
+    //private  String urlString ="https://samples.openweathermap.org/data/2.5/forecast?q=" + LOCATION + "&appid=" + API_KEY;
+    private  String urlString ="https://samples.openweathermap.org/data/2.5/weather?q=" + LOCATION + "&appid=" + API_KEY;
+
+
+    public WeatherHelper(){
+        try {
+            StringBuilder result = new StringBuilder();
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line=reader.readLine())!= null){
+                result.append(line);
+            }
+            reader.close();
+            System.out.println(result);
+
+            Map<String, Object> respMap = jsonToMap(result.toString());
+            //Map<String, Object> listMap = jsonToMap(respMap.get("list").toString());
+            Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString());
+
+            System.out.printf("Temperature is %.2f \n",  ((double)mainMap.get("temp")-273.17));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
